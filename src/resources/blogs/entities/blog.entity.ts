570e4e87150ffc658d81553b11b437dbc66f0323
dosepehr <dosepehr@gmail.com>
2025-03-27 18:39:02 +0330

    add blog entity

---
+++ src/resources/blogs/entities/blog.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn, DeleteDateColumn } from "typeorm";
+
+@Entity({ name: 'blogs' })
+export class Blog {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    image: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    writer: string
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
