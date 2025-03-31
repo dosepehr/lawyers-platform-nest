540ac3f140c2ac7f6b8077056d0d45b4686ac05b
dosepehr <dosepehr@gmail.com>
2025-03-31 14:00:42 +0330

    add blogs RBAC

--- src/resources/blogs/blogs.controller.ts
+++ src/resources/blogs/blogs.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator, UseGuards } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerConfig } from '../../utils/config/multer.config';
-
+import { JwtAuthGuard } from 'src/utils/guards/jwt-auth.guard';
+import { Roles } from 'src/utils/decorators/role.decorator';
+import { Role } from 'src/utils/enums/role.enum';
+import { RolesGuard } from 'src/utils/guards/roles.guard';
 @Controller('blogs')
 export class BlogsController {
   constructor(private readonly blogsService: BlogsService) { }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Post()
   @UseInterceptors(FileInterceptor('image', multerConfig))
   create(@Body() createBlogDto: CreateBlogDto, @UploadedFile(
@@ -32,22 +37,30 @@ export class BlogsController {
     return this.blogsService.findOne(slug);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Patch(':id')
   @UseInterceptors(FileInterceptor('image', multerConfig))
   update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto, @UploadedFile() file: Express.Multer.File) {
     return this.blogsService.update(+id, updateBlogDto, file);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Delete('/soft/:id')
   softDelete(@Param('id') id: string) {
     return this.blogsService.softDelete(+id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Delete('/hard/:id')
   hardDelete(@Param('id') id: string) {
     return this.blogsService.hardDelete(+id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Patch('/restore/:id')
   restore(@Param('id') id: string) {
     return this.blogsService.restore(+id);
